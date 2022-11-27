162632ce98840bc6272d95c3b0fcc6ff96373865
Christophe Billiard <christophe.billiard@gmail.com>
2022-11-27 18:37:11 +0100

    refacto: adopt a functional approach for easier testing and reusability

--- src/js/utils/pdfToImage.ts
+++ src/js/utils/pdfToImage.ts
@@ -1,24 +1,100 @@
-export const pdfToImage = (file, type, marginHeight) => {
-  const pages = [];
-  const heights = [];
-  let width = 0;
-  let height = 0;
-  let currentPage = 1;
+import * as pdfjsLib from "pdfjs-dist";
+// @ts-ignore
+import pdfWorkerProd from "pdfjs-dist/build/pdf.worker.min.js";
+// @ts-ignore
+import pdfWorkerDev from "pdfjs-dist/build/pdf.worker.js";
+
+
+function mergePages(
+  pages: [ImageData, number, number][],
+  marginHeight: number
+) {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  const totalHeight = pages.reduce(
+    (acc, [imageData, height, width], i) =>
+      acc + height + (i + 1 < pages.length ? marginHeight : 0),
+    0
+  );
+  const maxWidth = Math.max(
+    ...pages.map(([imageData, height, width]) => width)
+  );
+  canvas.width = maxWidth;
+  canvas.height = totalHeight;
+
+  for (let i = 0; i < pages.length; i++) {
+    ctx?.putImageData(
+      pages[i][0],
+      0,
+      pages
+        .slice(0, i)
+        .reduce(
+          (acc, [imageData, height, width]) => acc + height + marginHeight,
+          0
+        )
+    );
+  }
+
+  return canvas;
+}
+
+const pageToImageData = async (
+  page: any,
+  scale: number
+): Promise<[ImageData, number, number]> => {
+  const viewport = page.getViewport({ scale });
+
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+  const renderContext = { canvasContext: ctx, viewport };
+
+  // @ts-ignore
+  await page.render(renderContext).promise;
+
+  // @ts-ignore
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  return [imageData, canvas.height, canvas.width];
+};
+
+const convertCanvasToFile = (
+  canvas: HTMLCanvasElement,
+  filename: string,
+  type: string
+): File => {
+  const blobBin = atob(canvas.toDataURL(type).split(",")[1]);
+  const array = [];
+  for (let i = 0; i < blobBin.length; i++) {
+    array.push(blobBin.charCodeAt(i));
+  }
+  const blob = new Blob([new Uint8Array(array)], { type });
+  return new File([blob], filename, { type });
+};
+
+export const pdfToImage = (
+  file: File,
+  type: string,
+  marginHeight: number
+): Promise<File> => {
   const scale = 1.5;
 
-  function mergePages() {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    canvas.width = width;
-    canvas.height = height;
-    for (let i = 0; i < pages.length; i++) {
-      ctx.putImageData(pages[i], 0, heights[i]);
-    }
-    return canvas;
-  }
   return new Promise((resolve, reject) => {
-    if (typeof pdfjsLib === 'undefined') {
-      const message = 'The library PDF.js is required to convert PDF in image';
+    pdfjsLib.GlobalWorkerOptions.workerSrc = (() => {
+      if (process.env.NODE_ENV === "production") {
+        // use minified verion for production
+        return `/${pdfWorkerProd}`;
+      } else {
+        return `/${pdfWorkerDev}`;
+      }
+    })();
+
+    if (typeof pdfjsLib === "undefined") {
+      const message = "The library PDF.js is required to convert PDF in image";
       console.warn(message);
       reject(message);
       return;
@@ -27,46 +103,21 @@ export const pdfToImage = (file, type, marginHeight) => {
       const fileReader = new FileReader();
 
       fileReader.onload = async function () {
+        // @ts-ignore
         const typedarray = new Uint8Array(this.result);
 
         const pdf = await pdfjsLib.getDocument(typedarray).promise;
+        
 
-        getPage();
+        const pages = await Promise.all(
+          [...Array(pdf.numPages).keys()].map(
+            async (i) => pageToImageData(await pdf.getPage(i + 1), scale)
+          )
+        );
 
-        async function getPage() {
-          const page = await pdf.getPage(currentPage);
-
-          const viewport = page.getViewport({ scale });
-
-          const canvas = document.createElement("canvas");
-          const ctx = canvas.getContext("2d");
-          const renderContext = { canvasContext: ctx, viewport: viewport };
-
-          canvas.height = viewport.height;
-          canvas.width = viewport.width;
-
-          await page.render(renderContext).promise;
-          pages.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
-
-          heights.push(height);
-          height += canvas.height;
-          if (width < canvas.width) width = canvas.width;
-
-          if (currentPage < pdf.numPages) {
-            currentPage++;
-            height += marginHeight;
-            getPage();
-          } else {
-            const canvas = mergePages();
-            const blobBin = atob(canvas.toDataURL(type).split(",")[1]);
-            const array = [];
-            for (let i = 0; i < blobBin.length; i++) {
-              array.push(blobBin.charCodeAt(i));
-            }
-            const blob = new Blob([new Uint8Array(array)], { type });
-            resolve(new File([blob], file.name, { type }));
-          }
-        }
+        const canvas = mergePages(pages, marginHeight);
+        const resultFile = convertCanvasToFile(canvas, file.name, type);
+        resolve(resultFile);
       };
 
       fileReader.readAsArrayBuffer(file);
